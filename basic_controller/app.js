8f9671829a635713ed859208ca9c9484fd009d36
Charles Arciniega <charlesarcin@gmail.com>
2014-06-15 19:50:22 -0700

    uses expression to print language. uses correct cdn

--- basic_controller/app.js
+++ basic_controller/app.js
@@ -1,13 +1,13 @@
 (function(){
   var app = angular.module('basicApp', []);
+  var info = {
+    english: "Hello, AngularJS!",
+    spanish: "Hola, AngularJS!"
+  };
 
   app.controller('BasicController', function(){
     this.language = info;
   });
 
-  var info = {
-    english: "Hello, AngularJS!",
-    spanish: "Hola, AngularJS!"
-  };
 })();
 
