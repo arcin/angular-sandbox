27c3531f3184f11308b9c83a9fff38b0cb1916bd
Charles Arciniega <charlesarcin@gmail.com>
2014-06-16 11:31:38 -0700

    adds review to toys in app.js

--- basic_built_in_directives/app.js
+++ basic_built_in_directives/app.js
@@ -4,6 +4,7 @@
   { name: "pogo stick",
     price: 20.05,
     description: "It's a pogo stick.. it jumps",
+    review: "good",
     images: [
       "http://www.pogostickusa.com/imgaes_pogo.html/Junior%20Stinger%20Light%20Up%20Pogo%20Stick-RED.jpg",
       "http://blog.iqsdirectory.com/wp-content/uploads/files/springs%207.jpg"
@@ -11,6 +12,7 @@
   { name: "car",
     price: 45.75,
     description: "It's a car.. vroom vroom",
+    review: "fair",
     images: [
       "http://site.electric-scooters-galore.com/images/wheelsforkids/enzo.jpg",
       "http://eternalhobbyist.com/wp-content/uploads/2012/07/children_ride_on_car_toy_cars_kids_car_electronic__1257236924.jpg"
@@ -18,6 +20,7 @@
   { name: "dragon",
     price: 10,
     description: "It's a dragon. Like... from GoT",
+    review: "awesome",
     images: [
       "http://anwo.com/store/media/blue-ice-dragon-toy-miniature.jpg",
       "http://guidestobuy.com/images/actionfigure/upload//images/I/5146MSF5N9L.jpg"
