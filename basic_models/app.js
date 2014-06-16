e4e8bb05349e05d5b122fa847815bea2591ba414
Charles Arciniega <charlesarcin@gmail.com>
2014-06-16 15:14:27 -0700

    adds review controller and ratings

--- basic_models/app.js
+++ basic_models/app.js
@@ -1,3 +1,7 @@
 (function(){
   var app = angular.module('liveForm', []);
+  var ratings = [5, 4, 3, 2, 1];
+  app.controller('ReviewController', function(){
+    this.ratings = ratings;
+  });
 })();
