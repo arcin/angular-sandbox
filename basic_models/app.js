291ebebdc91ab4631768f681cbc7eb8127cf5c6b
Charles Arciniega <charlesarcin@gmail.com>
2014-06-16 15:35:50 -0700

    renames ratings to stars

--- basic_models/app.js
+++ basic_models/app.js
@@ -1,7 +1,7 @@
 (function(){
   var app = angular.module('liveForm', []);
-  var ratings = [5, 4, 3, 2, 1];
+  var stars = [5, 4, 3, 2, 1];
   app.controller('ReviewController', function(){
-    this.ratings = ratings;
+    this.stars = stars;
   });
 })();
