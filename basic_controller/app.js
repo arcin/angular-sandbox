82faa7972e4e26f8dc02eeddd3986edee5337455
Charles Arciniega <charlesarcin@gmail.com>
2014-06-15 19:27:34 -0700

    gives the basic controller some language properties

--- basic_controller/app.js
+++ basic_controller/app.js
@@ -2,7 +2,12 @@
   var app = angular.module('basicApp', []);
 
   app.controller('BasicController', function(){
-
+    this.language = info;
   });
+
+  var info = {
+    english: "Hello, AngularJS!",
+    spanish: "Hola, AngularJS!"
+  };
 })();
 
