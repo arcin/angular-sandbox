de215ca74bdabfe208f048ff5eb197e84e135fac
Charles Arciniega <charlesarcin@gmail.com>
2014-06-16 11:30:41 -0700

    adds descriptions to toys in app.js

--- basic_built_in_directives/app.js
+++ basic_built_in_directives/app.js
@@ -3,18 +3,21 @@
   var toys = [
   { name: "pogo stick",
     price: 20.05,
+    description: "It's a pogo stick.. it jumps",
     images: [
       "http://www.pogostickusa.com/imgaes_pogo.html/Junior%20Stinger%20Light%20Up%20Pogo%20Stick-RED.jpg",
       "http://blog.iqsdirectory.com/wp-content/uploads/files/springs%207.jpg"
     ]},
   { name: "car",
     price: 45.75,
+    description: "It's a car.. vroom vroom",
     images: [
       "http://site.electric-scooters-galore.com/images/wheelsforkids/enzo.jpg",
       "http://eternalhobbyist.com/wp-content/uploads/2012/07/children_ride_on_car_toy_cars_kids_car_electronic__1257236924.jpg"
     ]},
   { name: "dragon",
     price: 10,
+    description: "It's a dragon. Like... from GoT",
     images: [
       "http://anwo.com/store/media/blue-ice-dragon-toy-miniature.jpg",
       "http://guidestobuy.com/images/actionfigure/upload//images/I/5146MSF5N9L.jpg"
