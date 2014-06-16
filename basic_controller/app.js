ec63d539cd3836e558b39f4bfa40c9a1a58f4a54
Charles Arciniega <charlesarcin@gmail.com>
2014-06-15 21:27:14 -0700

    adds people controller. assigns people to array of people

--- basic_controller/app.js
+++ basic_controller/app.js
@@ -4,9 +4,18 @@
     english: "Hello, AngularJS!",
     spanish: "Hola, AngularJS!"
   };
+  var people = [
+  {name: 'charles', age: 21},
+  {name: 'sean', age: 20},
+  {name: 'juan', age: 33}
+  ];
 
   app.controller('BasicController', function(){
     this.language = info;
+  });
+
+  app.controller('PeopleController', function(){
+    this.people = people;
   });
 
 })();
